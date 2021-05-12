6fed629f2c1855ae249920545c3a2e7c938f6dc7
Nick S <nsardonini@gmail.com>
2021-05-12 16:15:13 -0400

    add container component for results of api call

---
+++ src/wordsContainer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import uniqid from "uniqid";
+
+const StyledContainer = styled.div`
+  .section-wrap {
+    padding: 10px 20px;
+
+    h2 {
+      font-size: 2rem;
+    }
+
+    div {
+      font-size: 1.1rem;
+    }
+  }
+`;
+
+export default function WordsContainer({ words }) {
+  const mapWords = () =>
+    Object.entries(words).map(([key, vals]) => (
+      <div className="section-wrap" key={uniqid()}>
+        <h2>{vals.length > 0 ? key : ""}</h2>
+        <div>
+          {vals.map((val, idx) => (
+            <React.Fragment key={uniqid()}>
+              {idx === vals.length - 1 ? val : val + ", "}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    ));
+  return <StyledContainer>{mapWords()}</StyledContainer>;
+}
